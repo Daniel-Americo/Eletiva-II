7001eb3ba2bdc6c6469fd3ca79206dd7a952a011
Daniel-Americo <daniel-americo@hotmail.com>
2025-09-06 14:07:25 -0300

    Refazendo todos exercicios, pois dei um revert commit e perdi tudo

---
+++ ProjetoTeste/Projeto/app/Http/Controllers/PrimeiraController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrimeiraController extends Controller
+{
+    public function exercicio1() {
+        return view("exercicio1");
+    }
+    public function respexercicio1(request $request) {
+        $valor1 = $request -> valor1;
+        $valor2 = $request -> valor2;
+        $soma = $valor1 + $valor2;
+        return "Soma dos valores $valor1 + $valor2 = $soma";
+    }
+
+    public function exercicio2() {
+        return view("exercicio2");
+    }
+    public function respexercicio2(request $request) {
+        $valor1 = $request -> valor1;
+        $valor2 = $request -> valor2;
+        $soma = $valor1 - $valor2;
+        return "Subtração dos valores $valor1 - $valor2 = $soma";
+    }
+
+    public function exercicio3() {
+        return view("exercicio3");
+    }
+    public function respexercicio3(request $request) {
+        $valor1 = $request -> valor1;
+        $valor2 = $request -> valor2;
+        $soma = $valor1 * $valor2;
+        return "Subtração dos valores $valor1 * $valor2 = $soma";
+    }
+
+    public function exercicio4() {
+        return view("exercicio4");
+    }
+    public function respexercicio4(request $request) {
+        $valor1 = $request -> valor1;
+        $valor2 = $request -> valor2;
+        if($valor2 = 0)
+            return " D
+        $soma = $valor1 * $valor2;
+        return "Subtração dos valores $valor1 * $valor2 = $soma";
+    }
+}
--- ProjetoTeste/Projeto/routes/web.php
+++ ProjetoTeste/Projeto/routes/web.php
@@ -17,21 +17,63 @@ Route::resource('clientes', ClienteController::class);
 
 
 
-
-
-
-
-
-
-
-
-
-
-Route::get('/teste', [PrimeiraController::class, "teste"]);
-
-Route::get('/exemplo', [PrimeiraController::class, "abrirForm"]);
-Route::post('/exemplo_resposta', [PrimeiraController::class, "resposta"]);
+Route::get("/exercicio1", [PrimeiraController::class, "exercicio1"]);
+Route::post("/respexercicio1", [PrimeiraController::class, "respexercicio1"]);
 
 Route::get("/exercicio2", [PrimeiraController::class, "exercicio2"]);
-Route::post("/resposta_exercicio2", [PrimeiraController::class, "respExercicio2"]);
+Route::post("/respexercicio2", [PrimeiraController::class, "respexercicio2"]);
+
+Route::get("/exercicio3", [PrimeiraController::class, "exercicio3"]);
+Route::post("/respexercicio3", [PrimeiraController::class, "respexercicio3"]);
+
+Route::get("/exercicio4", [PrimeiraController::class, "exercicio4"]);
+Route::post("/respexercicio4", [PrimeiraController::class, "respexercicio4"]);
+
+Route::get("/exercicio5", [PrimeiraController::class, "exercicio5"]);
+Route::post("/respexercicio5", [PrimeiraController::class, "respexercicio5"]);
+
+Route::get("/exercicio6", [PrimeiraController::class, "exercicio6"]);
+Route::post("/respexercicio6", [PrimeiraController::class, "respexercicio6"]);
+
+Route::get("/exercicio7", [PrimeiraController::class, "exercicio7"]);
+Route::post("/respexercicio7", [PrimeiraController::class, "respexercicio7"]);
+
+Route::get("/exercicio8", [PrimeiraController::class, "exercicio8"]);
+Route::post("/respexercicio8", [PrimeiraController::class, "respexercicio8"]);
+
+Route::get("/exercicio9", [PrimeiraController::class, "exercicio9"]);
+Route::post("/respexercicio9", [PrimeiraController::class, "respexercicio9"]);
+
+Route::get("/exercicio10", [PrimeiraController::class, "exercicio10"]);
+Route::post("/respexercicio10", [PrimeiraController::class, "respexercicio10"]);
+
+Route::get("/exercicio11", [PrimeiraController::class, "exercicio11"]);
+Route::post("/respexercicio11", [PrimeiraController::class, "respexercicio11"]);
+
+Route::get("/exercicio12", [PrimeiraController::class, "exercicio12"]);
+Route::post("/respexercicio12", [PrimeiraController::class, "respexercicio12"]);
+
+Route::get("/exercicio13", [PrimeiraController::class, "exercicio13"]);
+Route::post("/respexercicio13", [PrimeiraController::class, "respexercicio13"]);
+
+Route::get("/exercicio14", [PrimeiraController::class, "exercicio14"]);
+Route::post("/respexercicio14", [PrimeiraController::class, "respexercicio14"]);
+
+Route::get("/exercicio15", [PrimeiraController::class, "exercicio15"]);
+Route::post("/respexercicio15", [PrimeiraController::class, "respexercicio15"]);
+
+Route::get("/exercicio16", [PrimeiraController::class, "exercicio16"]);
+Route::post("/respexercicio16", [PrimeiraController::class, "respexercicio16"]);
+
+Route::get("/exercicio17", [PrimeiraController::class, "exercicio17"]);
+Route::post("/respexercicio17", [PrimeiraController::class, "respexercicio17"]);
+
+Route::get("/exercicio18", [PrimeiraController::class, "exercicio18"]);
+Route::post("/respexercicio18", [PrimeiraController::class, "respexercicio18"]);
+
+Route::get("/exercicio19", [PrimeiraController::class, "exercicio19"]);
+Route::post("/respexercicio19", [PrimeiraController::class, "respexercicio19"]);
+
+Route::get("/exercicio20", [PrimeiraController::class, "exercicio20"]);
+Route::post("/respexercicio20", [PrimeiraController::class, "respexercicio20"]);
 
